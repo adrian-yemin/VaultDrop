d85f924f2fedbf5caa668d8fba0bccdda78d466c
adrian-yemin <adrian.yemin@icloud.com>
2025-08-17 11:03:31 -0400

    Basic frontend to upload files and receive secure tokenized download link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [file, setFile] = useState(null)
+  const [uploadLink, setUploadLink] = useState(null);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>VaultDrop</h1>
+      <input type='file' onChange={(e) => setFile(e.target.files[0])}></input>
+      <button 
+        onClick={() => {
+          if(file){
+            const formData = new FormData()
+            formData.append("file", file)
+            formData.append("oneTimeUse", true)
+            formData.append("expiresAt", "2025-08-20T12:00:00Z")
+            fetch('http://localhost:8080/api/upload', {
+              method: 'POST',
+              body: formData
+            })
+            .then(response => {
+              if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+              }
+              return response.json();
+            })
+            .then(data => {
+              console.log("Upload successful:", data);
+              setUploadLink(data.link)
+            })
+            .catch(err => {
+              console.error("Upload failed:", err);
+            });
+          }
+          else{
+            console.log("No File Selected Yet")
+          }
+        }}
+      >
+        Upload
+      </button>
+      {uploadLink && (
+        <div>
+          <p>Your file has been uploaded!</p>
+          <a href={uploadLink} target="_blank" rel="noopener noreferrer">
+          {uploadLink}
+          </a>
+        </div>
+      )}
+    </div>
   )
 }
 
