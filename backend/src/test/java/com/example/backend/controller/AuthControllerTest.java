c6a48f69db3d66beb36c5f8176c610d61de585f5
adrian-yemin <adrian.yemin@icloud.com>
2025-12-24 18:13:45 -0500

    Added auth controller unit tests

---
+++ backend/src/test/java/com/example/backend/controller/AuthControllerTest.java
@@ -0,0 +1,56 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.request.AuthRequest;
+import com.example.backend.service.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(
+        controllers = AuthController.class,
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = "com\\.example\\.backend\\.config\\.security\\..*"
+        )
+)
+@AutoConfigureMockMvc(addFilters = false)
+public class AuthControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AuthService authService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void login_success() throws Exception {
+        AuthRequest request = new AuthRequest("user@example.com", "password");
+        String fakeToken = "jwt-token";
+
+        when(authService.loginUser(any(AuthRequest.class)))
+                .thenReturn(fakeToken);
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.message").value("Login successful"))
+                .andExpect(jsonPath("$.data").value(fakeToken));
+    }
+}
